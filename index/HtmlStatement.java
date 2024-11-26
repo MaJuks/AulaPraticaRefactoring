8ba0110c8fac762c07120f62794bbdb317fb62b3
Maria Júlia <maria.goncalves@eemovel.com>
2024-11-26 20:10:17 -0300

    COMMIT 14

---
+++ index/HtmlStatement.java
@@ -0,0 +1,17 @@
+public class HtmlStatement extends Statement {
+    @Override
+    protected String headerString(Customer customer) {
+        return "<H1>Rentals for <EM>" + customer.getName() + "</EM></H1><P>\n";
+    }
+
+    @Override
+    protected String rentalString(Rental rental) {
+        return rental.getMovie().getTitle() + ": " + rental.getCharge() + "<BR>\n";
+    }
+
+    @Override
+    protected String footerString(Customer customer) {
+        return "<P>You owe <EM>" + customer.getTotalCharge() + "</EM><P>\n" +
+               "On this rental you earned <EM>" + customer.getTotalFrequentRenterPoints() + "</EM> frequent renter points<P>";
+    }
+}
