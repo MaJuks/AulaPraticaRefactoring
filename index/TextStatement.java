6d9c1859467215805b7bfe26d7995a4b8533c955
Maria Júlia <maria.goncalves@eemovel.com>
2024-11-26 20:09:41 -0300

    COMMIT 13

---
+++ index/TextStatement.java
@@ -0,0 +1,17 @@
+public class TextStatement extends Statement {
+    @Override
+    protected String headerString(Customer customer) {
+        return "Rental Record for " + customer.getName() + "\n";
+    }
+
+    @Override
+    protected String rentalString(Rental rental) {
+        return "\t" + rental.getMovie().getTitle() + "\t" + rental.getCharge() + "\n";
+    }
+
+    @Override
+    protected String footerString(Customer customer) {
+        return "Amount owed is " + customer.getTotalCharge() + "\n" +
+               "You earned " + customer.getTotalFrequentRenterPoints() + " frequent renter points";
+    }
+}
